183e9059053c4e5c91036dad7877091ee0a004e2
palmtopturtle <gieske.jeff@gmail.com>
2018-11-19 22:10:20 -0500

    Transformed callbacks into promises, added comments

--- src/bot.js
+++ src/bot.js
@@ -12,26 +12,171 @@ const REDDIT_API_URL =
 const T = new Twit(config),
   client = redis.createClient();
 
-let activePost = null,
-  max = 0;
+let max = 0;
 
 client.on("connect", () => console.log("Connected to Redis!"));
 client.on("error", err => console.log(`Error connecting to Redis...\n${err}`));
 
-function beginProcess() {
-  request.get(REDDIT_API_URL, (err, res, body) => {
-    console.log("Fetching new posts from Reddit...");
-    if (err) {
-      console.log(`Error getting posts!\n${err}`);
-    } else {
-      let json = JSON.parse(body);
-      max = json.data.dist;
-      let posts = json.data.children.map(post => post.data);
-      getRandomPost(posts);
-    }
+/**
+ * Makes a call to the Reddit API and returns an array of posts.
+ */
+function getRedditPosts() {
+  return new Promise((resolve, reject) => {
+    console.log("Fetching posts from Reddit...");
+    request.get(REDDIT_API_URL, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        let json = JSON.parse(body);
+        max = json.data.dist;
+        let posts = json.data.children.map(post => post.data);
+        console.log("Success! Checking for valid post...");
+        resolve(posts);
+      }
+    });
   });
 }
 
+/**
+ * Checks to see if the current ID already exists in the database.
+ * @param {number} id
+ */
+function checkId(id) {
+  return new Promise((resolve, reject) => {
+    client.exists(id, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+/**
+ * Checks to see if a post is valid, as in:
+ * * The ID is unique and hasn't been used already
+ * * A preview exists for it
+ * * It does not include the word "help" in its title
+ * * It does not feature the "help" tag
+ * @param {array} posts
+ */
+function validatePost(posts) {
+  return new Promise((resolve, reject) => {
+    let newPost = getRandomPost(posts);
+    checkId(newPost.id)
+      .then(res => {
+        if (
+          newPost.url &&
+          !res &&
+          !newPost.title.toLowerCase().includes("help") &&
+          newPost.tag !== "Help"
+        ) {
+          console.log("Post found!");
+          client.set(newPost.id, newPost.id, "EX", 172800); //Expires after 48 hours
+          resolve(newPost);
+        } else {
+          console.log("Post invalid, trying again...");
+          resolve(validatePost(posts));
+        }
+      })
+      .catch(err => reject(err));
+  });
+}
+
+/**
+ * Downloads a high quality version of the image from the post.
+ * @param {object} post
+ */
+function downloadImage(post) {
+  return new Promise((resolve, reject) => {
+    console.log("Downloading image...");
+    let fileExt = path.extname(post.url);
+    let testForArgs = fileExt.indexOf("?");
+    if (testForArgs) fileExt = fileExt.substring(0, testForArgs);
+    let fileName = `succulent${fileExt}`;
+    let file = fs.createWriteStream(fileName);
+    request(post.url)
+      .pipe(file)
+      .on("close", err => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("Download complete!");
+          resolve({ post: post, image: filePath });
+        }
+      });
+  });
+}
+
+/**
+ * Uploads the image to Twitter and creates the parameters (tweet data) associated with it.
+ * @param {object} postData
+ */
+function uploadImage(postData) {
+  return new Promise((resolve, reject) => {
+    console.log("Uploading image to Twitter...");
+    let filePath = path.join(__dirname, `../${postData.image}`);
+    T.postMediaChunked({ file_path: filePath }, (err, data, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log("Upload successful!");
+        let params = {
+            status: `Source: /u/${postData.post.author} [${
+              postData.post.permalink
+            }]`,
+            media_ids: [data.media_id_string]
+          },
+          meta_params = {
+            media_id: data.media_id_string,
+            alt_text: { text: postData.post.title }
+          };
+        resolve({ params: params, meta_params: meta_params });
+      }
+    });
+  });
+}
+
+/**
+ * Posts the meta data and tweet content to Twitter.
+ * @param {object} media
+ */
+function createTweet(media) {
+  return new Promise((resolve, reject) => {
+    T.post("media/metadata/create", media.meta_params, (err, data, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        T.post("statuses/update", media.params, (err, data, res) => {
+          console.log("Successfully posted tweet!");
+          resolve();
+        });
+      }
+    });
+  });
+}
+
+/**
+ * If all promises are resolved, outputs useful statistics.
+ */
+function outputSuccess() {
+  return new Promise((resolve, reject) => {
+    client.dbsize((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`Number of post IDs in database: ${res}`);
+        console.log("Process completed, awaiting next call...\n-------------");
+      }
+    });
+  });
+}
+
+/**
+ * Utility function that randomly chooses a post.
+ * @param {array} posts
+ */
 function getRandomPost(posts) {
   let num = Math.floor(Math.random() * (max - 1));
   let newPost = {
@@ -42,80 +187,20 @@ function getRandomPost(posts) {
     author: posts[num].author,
     tag: posts[num].link_flair_text
   };
-
-  client.exists(newPost.id, (err, res) => {
-    console.log(`Result of ID comparison: ${res}, checking other parameters...`)
-    if (
-      newPost.url &&
-      !res &&
-      !newPost.title.toLowerCase().includes("help") &&
-      newPost.tag !== "Help"
-    ) {
-      console.log("Post found!");
-      activePost = newPost;
-      client.set(activePost.id, activePost.id, "EX", 172800); //48 hours
-      saveImage();
-    } else {
-      console.log("Post invalid, trying again...");
-      getRandomPost(posts);
-    }
-  });
+  return newPost;
 }
 
-function saveImage() {
-  let imageFileExt = path.extname(activePost.url);
-  let testForArgs = imageFileExt.indexOf("?");
-  if (testForArgs) imageFileExt = imageFileExt.substring(0, testForArgs);
-
-  let imageFileName = `succulent${imageFileExt}`;
-  let imageFile = fs.createWriteStream(imageFileName);
-
-  console.log("Downloading image...");
-  request(activePost.url)
-    .pipe(imageFile)
-    .on("close", err => {
-      if (err) {
-        console.log(`Error downloading image!\n${err}`);
-      } else {
-        console.log("Download successful!");
-        uploadImage(imageFileName);
-      }
-    });
-}
-
-function uploadImage(imageFileName) {
-  let filePath = path.join(__dirname, `../${imageFileName}`);
-  console.log(`Uploading image from path "${filePath}"...`);
-  T.postMediaChunked({ file_path: filePath }, (err, data, res) => {
-    if (err) {
-      console.log(`Error uploading image to Twitter!\n${err}`);
-    } else {
-      console.log("Upload successful!");
-      let params = {
-        status: `Source: /u/${activePost.author} [${activePost.permalink}]`,
-        media_ids: [data.media_id_string]
-      };
-      let meta_params = {
-        media_id: data.media_id_string,
-        alt_text: { text: activePost.title }
-      };
-      createTweet(params, meta_params);
-    }
-  });
-}
-
-function createTweet(params, meta_params) {
-  T.post("media/metadata/create", meta_params, (err, data, res) => {
-    if (err) {
-      console.log(`Error creating tweet!\n${err}`);
-    } else {
-      T.post("statuses/update", params, (err, data, res) => {
-        console.log("Successfully created tweet! Process completed.");
-        console.log("Awaiting next call...");
-        client.dbsize((err, res) => console.log(`Number of post IDs in database: ${res}`));
-      });
-    }
-  });
+/**
+ * Promise chain that goes through the whole process, from calling the Reddit API to actually posting the tweet.
+ */
+function beginProcess() {
+  getRedditPosts()
+    .then(posts => validatePost(posts))
+    .then(post => downloadImage(post))
+    .then(data => uploadImage(data))
+    .then(media => createTweet(media))
+    .then(() => outputSuccess())
+    .catch(err => console.log(err));
 }
 
 beginProcess();
